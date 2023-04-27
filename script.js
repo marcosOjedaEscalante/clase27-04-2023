a433e4c58bfd829ecba59ee23a809c785360e13b
Marcos Ojeda Escalante <marcos.ojeda.escalante@gmail.com>
2023-04-27 19:47:19 -0400

    base proyecto

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Arreglos */
+/* Son una lista de datos */
+/* Tienen un largo dinámico */
+/* Se recorren con un for o con cualquier ciclo */
+/* Se definen entre corchetes y cada elemento va separado por coma */
+/* Tienen una posición y un índice */
+/* Pueden tener un subelemento o mas */
+/* Podemos guardar funciones, objetos o distintas estructuras de datos */
+/* Atributo largo = lenght (tienen atributos) */
+/* Métodos (funciones) = representan acciones o procesos */
+
