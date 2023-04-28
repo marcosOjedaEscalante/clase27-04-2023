2fbf785bdf55b218eeddcadbc9d4396cac81aca2
Marcos Ojeda Escalante <marcos.ojeda.escalante@gmail.com>
2023-04-27 22:02:07 -0400

    for, for of, forEach, intro filter

--- scriptDos.js
+++ scriptDos.js
@@ -4,6 +4,35 @@ function consumoDeAPI(){
         url: "https://pokeapi.co/api/v2/pokemon",
         success: function (response) {
             console.log(response);
+            /* for(var i = 0 ; i < response.results.length ; i = i + 1){
+                console.log(response.results[i]);
+            } */
+
+            for(var pokemon of response.results){
+                console.log(pokemon.url);
+            }
+
+            /* forEach, map, filter */
+            /* forEach */
+            /* Con defición de función anterior */
+            function callback(pokemon, index){
+                console.log(pokemon + " " + index);
+            }
+
+            response.results.forEach(callback);
+
+            /* Con función anónima */
+            response.results.forEach(function(pokemon, index){
+                console.log(pokemon + " " + index);
+            });
+            
+            /* Filter */
+            var filtrados = response.results.filter(function(pokemon){
+                return pokemon.name == 'metapod';
+            });
+            
+            console.log(filtrados);
+
         }
     });
 }
